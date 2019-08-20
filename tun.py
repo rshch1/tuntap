2973e921eac3e16417d4e2970442f9b730ea57f4
rshch <mr_riki@mail.ru>
2019-08-20 09:12:21 +0600

    tun.py added

---
+++ tun.py
@@ -0,0 +1,9 @@
+from tuntap import TunTap
+
+
+tun = TunTap(nic_type='Tun', nic_name='mytun')
+tap = TunTap(nic_type='Tap', nic_name='mytap')
+tap.config(ip="192.168.137.1",mask="255.255.255.240")
+buf = tun.read(1500)
+print(tun.name,tun.ip,tun.mask)
+tun.write(buf)
