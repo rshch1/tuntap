f44d31db7e6c90be50b58f6e8f99dafefd82a529
rshch <mr_riki@mail.ru>
2019-08-22 16:41:26 +0600

    tun/tap library removed

--- tun.py
+++ tun.py
@@ -1,8 +1,23 @@
-from tuntap import TunTap
+import os
+from fcntl import ioctl
+import struct
+from pyroute2 import IPRoute
+
+TUNSETIFF = 0x400454ca
+IFF_TUN   = 0x0001
+IFF_NO_PI = 0x1000
+
+ftun = os.open("/dev/net/tun", os.O_RDWR)
+ioctl(ftun, TUNSETIFF, struct.pack("16sH", b"tun0", IFF_TUN | IFF_NO_PI))
 
 
-tun = TunTap(nic_type='Tun', nic_name='mytun')
-tap = TunTap(nic_type='Tap', nic_name='mytap')
-tap.config(ip="192.168.137.1",mask="255.255.255.240")
-buf = tun.read(1500)
-tun.write(buf)
+ip = IPRoute()
+idx = ip.link_lookup(ifname='tun0')[0]
+ip.addr('add', index=idx, address='192.168.137.1', prefixlen=28)
+ip.link('set', index=idx, state='up')
+while True:
+    packet = list(os.read(ftun, 2048))
+    packet[12:16], packet[16:20] = packet[16:20], packet[12:16]
+    os.write(ftun, ''.join(packet))
+ip.close()
+
